8bd34b555daa314c8c99ce0897e690159c50315e
ZhengBaoLi <418736259@qq.com>
2017-08-25 14:50:47 +0800

    add Device Language

--- ZHCategoty/Category/UIDevice+ZHExtension.h
+++ ZHCategoty/Category/UIDevice+ZHExtension.h
@@ -11,3 +11,16 @@
 @interface UIDevice (ZHExtension)
 
 @end
+
+
+typedef NS_ENUM(NSInteger, ZHDeviceLanguage) {
+    ZHDeviceLanguageEn_US     = 0,  //英文
+    ZHDeviceLanguageZh_Hans_US      = 1,  //简体中文
+    ZHDeviceLanguageZh_Hant_US      = 2,   //繁体中文
+    ZHDeviceLanguageUnknown      = 3,
+};
+@interface UIDevice (ZHLanguageExtension)
+
+- (ZHDeviceLanguage)zh_currentDeviceLanguage;
+
+@end
--- ZHCategoty/Category/UIDevice+ZHExtension.m
+++ ZHCategoty/Category/UIDevice+ZHExtension.m
@@ -11,3 +11,33 @@
 @implementation UIDevice (ZHExtension)
 
 @end
+
+@implementation UIDevice (ZHLanguageExtension)
+
+- (ZHDeviceLanguage)zh_currentDeviceLanguage{
+    NSString *preferredLang = [[[NSUserDefaults standardUserDefaults] objectForKey:@"AppleLanguages"] objectAtIndex:0];
+    
+    ZHDeviceLanguage zhDeviceLanguage;
+    
+    if ([preferredLang isEqualToString:@"en-US"]) {
+        
+        zhDeviceLanguage = ZHDeviceLanguageEn_US;
+        
+    }else if ([preferredLang isEqualToString:@"zh-Hans-US"]){
+        
+        zhDeviceLanguage = ZHDeviceLanguageZh_Hans_US;
+        
+    }else if ([preferredLang isEqualToString:@"zh-Hant-US"]){
+        
+        zhDeviceLanguage = ZHDeviceLanguageZh_Hant_US;
+        
+    }else{
+        
+        zhDeviceLanguage = ZHDeviceLanguageUnknown;
+        
+    }
+    return zhDeviceLanguage;
+}
+
+@end
+
